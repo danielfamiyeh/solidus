1a21626965f3128a84435398492ac2b5d9089508
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-02-10 12:34:00 +0000

    Add Post module

--- backend/contracts/Solidus.sol
+++ backend/contracts/Solidus.sol
@@ -1,26 +1,31 @@
 // contracts/Solidus.sol
 // SPDX-License-Identifier: MIT
 
+import './SolidusPost.sol';
 import './SolidusUser.sol';
 
 pragma solidity ^0.8.0;
 
-contract Solidus is SolidusUser {
+contract Solidus is SolidusUser, SolidusPost {
   address private _owner;
   
-  struct Post {
-    address from;
-    PostContent[] content;
-  }
-
-  struct PostContent {
-    string postText;
-    string postImage;
-  }
-
   constructor(){
     _owner = msg.sender;
   }
 
-  
+  function getPosts(address user) public view onlyWithAccount returns(Post[] memory)  {
+    return _getPosts(user);
+  } 
+  function getPost(address user, bytes32 uuid) public view onlyWithAccount returns(Post memory post, uint index) {
+    return _getPost(user, uuid);
+  }
+  function createPost( PostContent[] calldata postContent) public onlyWithAccount {
+    _createPost(postContent);
+  } 
+  function updatePost(bytes32 id, PostContent[] calldata postContent) public onlyWithAccount {
+    _updatePost(id, postContent);
+  }
+  function deletePost(bytes32 id) public onlyWithAccount {
+    _deletePost(id);
+  }
 }
