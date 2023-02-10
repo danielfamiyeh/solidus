775797e9b1fc5c42cd1a4971b38b270e7a013ef4
danielfamiyeh <daniel.kwaku.famiyeh@gmail.com>
2023-02-10 19:23:41 +0000

    Start user test suite

--- backend/test/SolidusUser.test.js
+++ backend/test/SolidusUser.test.js
@@ -1,6 +1,27 @@
 const { assert } = require('chai');
 const { ethers } = require('hardhat');
 
-describe('Solidus user test suite', function () {
-  let deployer;
+describe('User test suite', function () {
+  let deployer, impersonateDeployer, testSolidusContract;
+
+  this.beforeEach(async () => {
+    const testSolidusFactory = await ethers.getContractFactory('Solidus', 0);
+    testSolidusContract = await testSolidusFactory.deploy();
+
+    await testSolidusContract.deployed();
+    deployer = await testSolidusContract.getOwner();
+
+    await hre.network.provider.request({
+      method: 'hardhat_impersonateAccount',
+      params: [deployer.toString()],
+    });
+
+    impersonateDeployer = await ethers.getSigner(deployer.toString());
+  });
+
+  describe('CRUD methods', () => {
+    it('should', () => {
+      console.log({ deployer });
+    });
+  });
 });
